46604838559f3d8accb280205fbf01dd54fd0164
Elvanss <duy.elvans12@gmail.com>
2025-01-15 22:21:36 +1100

    set up database migration

--- user-service/src/main/java/com/banking/microservice/userservice/domain/entities/User.java
+++ user-service/src/main/java/com/banking/microservice/userservice/domain/entities/User.java
@@ -1,21 +1,31 @@
 package com.banking.microservice.userservice.domain.entities;
 
 import com.banking.microservice.userservice.types.Roles;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.GenericGenerator;
+
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
+@Getter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    @ColumnDefault("random_uuid()")
     private UUID id;
 
     @Column(name = "email" , nullable = false)
@@ -32,6 +42,13 @@ public class User {
 
     @Column(name = "is_active" , nullable = false)
     private boolean isActive;
+
     @Column(name = "role" , nullable = false)
     private Roles role;
+
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt", nullable = false)
+    private LocalDateTime updatedAt;
 }
--- user-service/src/main/java/com/banking/microservice/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/banking/microservice/userservice/repository/UserRepository.java
@@ -3,12 +3,14 @@ package com.banking.microservice.userservice.repository;
 import com.banking.microservice.userservice.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    @Query("SELECT u FROM User u WHERE u.email =: email")
+    @Query("SELECT u FROM User u WHERE u.email = :email")
     Optional<User> findByEmail(String email);
 }
